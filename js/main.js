0d770708e0ff9374977d31362a69ec730c7d5db2
Ihor0809 <Ihor08092003@gmail.com>
2023-01-15 01:13:39 +0300

    Landing page lambo

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open');
+});
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      }
+    }
+});
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.23209731255191, lng: -8.628293850144738 },
+    zoom: 14,
+  });
+}
+
+window.initMap = initMap;
